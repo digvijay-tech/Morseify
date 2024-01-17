f8a53d24eba41b03a9e7b46d9d25923bd53f21d5
digvijay-webdev <djpadhiyar881@gmail.com>
2024-01-17 02:35:10 +0000

    portrait orientation locked..

--- morseify_app/lib/main.dart
+++ morseify_app/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:morseify_app/app.dart';
 
 void main() {
   // initialize services and configurations before the app starts
   WidgetsFlutterBinding.ensureInitialized();
+
+  // enforcing portrait orientation for all devices
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(const MorseifyApp(isDevelopment: false));
 }
